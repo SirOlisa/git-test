5f0be7d02ac37cc39368834c84056250d80d8b13
SirOlisa <olisa2106@gmail.com>
2025-03-22 18:37:33 +0100

    This is a declaration and a promise to myself

---
+++ devopsdeclaration.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Declare Variables
+user_name="Olisa"
+career_field="DevOps"
+
+#Hyping Myself Up
+echo "Initializing success... "
+sleep 1
+echo "Loading determination... "
+sleep 1
+echo "Executing greatness... "
+
+#Define My Declaration
+success_statement="$user_name will absolutely crush it in $career_field and become a legend!"
+
+#Execute My Declaration
+echo "$success_statement"
+
+
+
+
+
+
+
